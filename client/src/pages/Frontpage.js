4786cfee894cdcbb74934e200acbd6251a8ef1c0
Christian Goldapp <c.goldapp@tu-braunschweig.de>
2020-12-07 15:18:53 +0100

    Fix git setup

---
+++ client/src/pages/Frontpage.js
@@ -0,0 +1,5 @@
+const React = require("react");
+
+export default class Frontpage extends React.Component{
+
+}
---
+++ client/src/pages/Scripts.js
@@ -0,0 +1,5 @@
+const React = require("react");
+
+export default class Scripts extends React.Component{
+
+}
