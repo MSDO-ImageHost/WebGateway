053c324daff61b0e6071a71370bb6807a21bf10f
Christian Goldapp <c.goldapp@tu-braunschweig.de>
2020-12-15 13:53:42 +0100

    Update routes and fix problems with test data

--- api/src/rest/Accounts.js
+++ api/src/rest/Accounts.js
@@ -5,14 +5,21 @@ const router = express.Router();
 const data = [{
     username: "Max Mustermann",
     email: "max.mustermann@firma.de",
-    role: "user",
+    role: "0",
     last_changed: "2020-12-14T13:16:36.080",
     created_at: "2020-12-13T13:16:36.080",
     last_login: "2020-12-15T13:16:36.080",
 }, {
     username: "Jens Jensen",
     email: "jens.jensen@selskab.dk",
-    role: "mod",
+    role: "10",
+    last_changed: "2020-12-14T13:16:36.080",
+    created_at: "2020-12-13T13:16:36.080",
+    last_login: "2020-12-15T13:16:36.080",
+}, {
+    username: "John Bull",
+    email: "john.bull@company.co.uk",
+    role: "20",
     last_changed: "2020-12-14T13:16:36.080",
     created_at: "2020-12-13T13:16:36.080",
     last_login: "2020-12-15T13:16:36.080",
--- api/src/rest/Authentication.js
+++ api/src/rest/Authentication.js
@@ -8,7 +8,7 @@ router.use(function timeLog(req, res, next) {
     next();
 });
 const jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NjU0MDMyMDAsImlhdCI6MTY2NTQwMzIwMCwiaXNzIjoiSW1hZ2VIb3N0LnNkdS5kayIsInJvbGUiOjAsInN1YiI6IjEyIn0.0KKTtjDmMjQ9uRryM5LGGYK5Ko_sDsuCH_PqSIPrD2I";
-router.post('/:user', function (req, res) {
+router.post('/', function (req, res) {
     const user = req.params.user;
     const password = req.params.password;
     console.log("Logging in as " + user + " with password " + password);
