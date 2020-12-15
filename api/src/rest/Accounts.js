7320228963da10fc763962883b67e6a9556cb354
Christian Goldapp <c.goldapp@tu-braunschweig.de>
2020-12-15 13:22:05 +0100

    Create REST placeholders

--- api/src/rest/Accounts.js
+++ api/src/rest/Accounts.js
@@ -2,16 +2,20 @@ const express = require("express");
 
 const router = express.Router();
 
-// middleware that is specific to this router
-router.use(function timeLog (req, res, next) {
-    console.log('Time: ', Date.now());
-    next();
-});
+const data = [{
+    username: "Max Mustermann",
+    email: "max.mustermann@firma.de",
+    role: "user",
+    last_changed: "2020-12-14T13:16:36.080",
+    created_at: "2020-12-13T13:16:36.080",
+    last_login: "2020-12-15T13:16:36.080",
+}];
+
 router.get('/', function (req, res) {
-    res.send('All accounts');
+    res.json(data)
 });
-router.post('/:user', function (req, res) {
-    res.send('Account data of' + req.params["user"]);
+router.get('/:user', function (req, res) {
+    res.send(data[req.params.user])
 });
 
 module.exports = router;
--- api/src/rest/Authentication.js
+++ api/src/rest/Authentication.js
@@ -3,15 +3,16 @@ const express = require("express");
 const router = express.Router();
 
 // middleware that is specific to this router
-router.use(function timeLog (req, res, next) {
+router.use(function timeLog(req, res, next) {
     console.log('Time: ', Date.now());
     next();
 });
-router.get('/', function (req, res) {
-    res.send('Logged in as:');
-});
+const jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NjU0MDMyMDAsImlhdCI6MTY2NTQwMzIwMCwiaXNzIjoiSW1hZ2VIb3N0LnNkdS5kayIsInJvbGUiOjAsInN1YiI6IjEyIn0.0KKTtjDmMjQ9uRryM5LGGYK5Ko_sDsuCH_PqSIPrD2I";
 router.post('/:user', function (req, res) {
-    res.send('Login attempt as user' + req.params["user"]);
+    const user = req.params.user;
+    const password = req.params.password;
+    console.log("Logging in as " + user + " with password " + password);
+    res.json({user, jwt})
 });
 
 module.exports = router;
