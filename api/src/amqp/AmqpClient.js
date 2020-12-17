414971f3d55e18b1bfdaa223402065aaaf887589
Lasse Sørensen <33939871+lass7965@users.noreply.github.com>
2020-12-17 01:39:54 +0100

    Changed rabbitmq exchange to direct

--- api/src/amqp/AmqpClient.js
+++ api/src/amqp/AmqpClient.js
@@ -4,7 +4,8 @@ const { v4: uuid } = require('uuid');
 
 // this queue name will be attached to "replyTo" property on producer's message,
 // and the consumer will use it to know which queue to the response back to the producer
-const REPLY_QUEUE = process.env.GATEWAY_QUEUE || "gateway-queue";
+const QUEUE = process.env.GATEWAY_QUEUE || "gateway-queue";
+const EXCHANGE = "rapid";
 
 /**
  * Create AMQP channel and return back as a promise
@@ -12,34 +13,51 @@ const REPLY_QUEUE = process.env.GATEWAY_QUEUE || "gateway-queue";
  * @params {String} setting.url
  * @returns {Promise} - return AMQP channel
  */
+
 const createClient = (setting) => AMQP.connect(setting.url)
-    .then(conn => {
-        return conn.createChannel()
-    }) // create channel
-    .then(channel => {
-        channel.assertQueue(REPLY_QUEUE, { durable: true });
-        channel.responseEmitter = new EventEmitter();
-        channel.responseEmitter.setMaxListeners(0);
-        channel.consume(REPLY_QUEUE,
-            msg => channel.responseEmitter.emit(msg.properties.correlationId, msg.content),
-            { noAck: true });
-        return channel;
+        .then(conn => {
+            console.log("Created channel!")
+            return conn.createChannel()
+        })
+        .then(channel => {
+            console.log("Created queue!")
+            channel.assertQueue(QUEUE, { durable: true });
+            console.log("Created exchange!")
+            channel.assertExchange(EXCHANGE, 'direct', {durable: false});
+            channel.responseEmitter = new EventEmitter();
+            channel.responseEmitter.setMaxListeners(0);
+            channel.consume(QUEUE, function(msg) {
+                console.log("[x] " + msg.properties);
+                channel.responseEmitter.emit(msg.properties.correlationId, msg.content);
+            } , { noAck: true } );
+            return channel;
+        });
+
+const bindQueue = (channel,events) => {
+    events.forEach(event =>{
+        channel.bindQueue(QUEUE, EXCHANGE, event);
     });
+}
 
 /**
- * Send RPC message to waiting queue and return promise object when
+ * Send event to direct exchange and return promise object when
  * event has been emitted from the "consume" function
  * @params {Object} channel - AMQP channel
  * @params {String} message - message to send to consumer
- * @params {String} rpcQueue - name of the queue where message will be sent to
+ * @params {String} event - name of the event
  * @returns {Promise} - return msg that send back from consumer
  */
-const sendRPCMessage = (channel, message, rpcQueue) => new Promise(resolve => {
-    const correlationId = uuid();
-    channel.assertQueue(rpcQueue, { durable: true });
-    channel.responseEmitter.once(correlationId, resolve);
-    channel.sendToQueue(rpcQueue, Buffer.from(message), { correlationId, replyTo: REPLY_QUEUE });
+const sendMessage = (channel, message, event) => new Promise(resolve => {
+    console.log(" [x] Sending " + event);
+    const cID = uuid();
+    channel.responseEmitter.once(cID, resolve);
+    var options = {
+        correlationId: cID,
+        contentType: "application/json"
+    };
+    channel.publish(EXCHANGE,event,Buffer.from(message), options);
 });
 
 module.exports.createClient = createClient;
-module.exports.sendRPCMessage = sendRPCMessage;
+module.exports.sendMessage = sendMessage;
+module.exports.bindQueue = bindQueue;
