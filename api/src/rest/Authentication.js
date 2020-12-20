5ecd7dd5e26184219d9b5cfcd3659bef28a3a00c
Anne Møller Madsen <33927314+anne389j@users.noreply.github.com>
2020-12-20 20:21:25 +0100

    added send to the endpoints of authenticaton

--- api/src/rest/Authentication.js
+++ api/src/rest/Authentication.js
@@ -14,37 +14,48 @@ const router = express.Router();
 // Creates a JWT for an a existing user (login) // Request body contains this: `{username:<String>, password:<String>}`
 router.post('', function (req, res) {
     // {"sub":"5","role":"user","iss":"ImageHost.sdu.dk","exp":1638560713,"iat":1607024713}
-    const token = JWT_ENCODE({sub:0, role:0, iss: "ImageHost.sdu.dk"});
-    findUser = { username:req.body.username, email:req.body.email, password:req.body.password }
-    found = TEST_USERS.find(user => {return user.username === findUser.username && user.password === findUser.password })
-    console.log(found)
+    //const token = JWT_ENCODE({sub:0, role:0, iss: "ImageHost.sdu.dk"});
+    //findUser = { username:req.body.username, email:req.body.email, password:req.body.password }
+    //found = TEST_USERS.find(user => {return user.username === findUser.username && user.password === findUser.password })
+    //console.log(found)
 
-    if (found == undefined) {
-        return res.status(401).send()
-    }
-    res.status(200).json({token, user:found});
-});
-
-// Updates a users password
-router.get('', validJWT, function (req, res) {
-    //RequestAccountPasswordUpdate
-    res.json({
-        token: req.jwt,
-        claims: req.claims
+    //if (found == undefined) {
+    //    return res.status(401).send()
+    //}
+    //res.status(200).json({token, user:found});
+    amqpClient.sendMessage(JSON.stringify(req.body),"RequestLoginToken").then(msg => {
+        const result = JSON.parse(msg.toString());
+        console.log("Received " + msg.toString());
+        res.json(result);
     });
 });
 
 // Updates a users password
+//router.get('', validJWT, function (req, res) {
+    //RequestAccountPasswordUpdate
+    //res.json({
+    //    token: req.jwt,
+    //    claims: req.claims
+    //});
+//});
+
+// Updates a users password
 router.put('', validJWT, function (req, res) {
     //RequestAccountPasswordUpdate
-    res.status(200).send();
+    // FOR LATER: jwt = req.cookies["_auth_t"]
+    amqpClient.sendMessage(JSON.stringify(req.body),"RequestAccountPasswordUpdate").then(msg => {
+        const result = JSON.parse(msg.toString());
+        console.log("Received " + msg.toString());
+        res.json(result);
+    });
 });
 
 
 // Terminates a users login session
+// Look at later! 
 router.delete('', validJWT, function (req, res) {
     //invalidate token
-    res.status(200).send();
+    
 });
 
 
