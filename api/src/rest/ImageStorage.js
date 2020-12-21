a0935c49bda6b6d3ac6e4909044e913b9f7f0b33
Christian Goldapp <c.goldapp@tu-braunschweig.de>
2020-12-21 21:47:48 +0100

    Divide ImageStorage.js into two routers

--- api/src/rest/ImageStorage.js
+++ api/src/rest/ImageStorage.js
@@ -2,59 +2,46 @@ const express = require("express");
 const {validJWT, maybeJWT} = require("../jwtAuth");
 const amqpClient = require("../amqp/AmqpClient");
 
-const router = express.Router();
+const apiRouter = express.Router();
 
-amqpClient.bindQueue(["ImageLoadRequest", "ImageCreateRequest", "ImageDeleteRequest"]);
+amqpClient.bindQueue(["ImageLoadResponse"]);
 
-router.post('/image', validJWT, function (req, res) {
-    //Creates an image
+apiRouter.get('/:iid', function (req, res) {
+    //Gets an image using its id
     var token = {
-        "jwt":req.cookies["_auth_t"]
+        "jwt": req.cookies["_auth_t"]
     }
-    amqpClient.sendMessage(JSON.stringify(req.body),"ImageCreateResponse",token).then(msg => {
-        if(msg.properties.headers.http_response === 200){
+    amqpClient.sendMessage(JSON.stringify({"post_id": req.params.iid}), "ImageLoadRequest", token).then(msg => {
+        if (msg.properties.headers.http_response === 200) {
             const result = msg.content.toString();
-            console.log("Received " + msg.content.toString());
             res.json(result);
-        }
-        else{
-            // I am assuming you store a error message inside properties.headers.message, change if it is not true.
-            // Also has to change is it is status_code, http_response, or a third thing.
+        } else {
             res.status(msg.properties.headers.status_code).send(msg.properties.headers.message);
         }
     });
 });
-router.get('/image/:iid', function (req, res) {
+
+const imageRouter = express.Router();
+
+/**
+ * imageRouter.get('/:path', function (req, res) {
+    let iid = req.params.path.split("\.")[0];
     //Gets an image using its id
     var token = {
         "jwt":req.cookies["_auth_t"]
     }
-    amqpClient.sendMessage(JSON.stringify(req.body),"ImageLoadResponse",token).then(msg => {
+    amqpClient.sendMessage(JSON.stringify({"post_id": iid}),"ImageLoadRequest",token).then(msg => {
         if(msg.properties.headers.http_response === 200){
             const result = msg.content.toString();
-            console.log("Received " + msg.content.toString());
-            res.json(result);
+            const b64 = JSON.parse(result).image_data;
+            const data = Buffer.from(b64, 'base64');
+            res.status(200).
         }
         else{
             res.status(msg.properties.headers.status_code).send(msg.properties.headers.message);
         }
     });
 });
-router.delete('/image/:iid', validJWT, function (req, res) {
-    //Deletes an image using its id.
-    var token = {
-        "jwt":req.cookies["_auth_t"]
-    }
-    amqpClient.sendMessage(JSON.stringify(req.body),"ImageDeleteResponse",token).then(msg => {
-        if(msg.properties.headers.http_response === 200){
-            const result = msg.content.toString();
-            console.log("Received " + msg.content.toString());
-            res.json(result);
-        }
-        else{
-            res.status(msg.properties.headers.status_code).send(msg.properties.headers.message);
-        }
-    });
-});
+ */
 
-module.exports = router;
+module.exports = {api: apiRouter, images: imageRouter};
