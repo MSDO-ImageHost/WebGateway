39f5bc536058ba290415ed5b62f15abf0879aaae
Giz <Jeppe1000@live.dk>
2020-12-22 00:09:56 +0100

    Updated comment's endpoints

--- api/src/rest/Comments.js
+++ api/src/rest/Comments.js
@@ -13,7 +13,10 @@ router.get('/:cid', function (req, res) {
     var token = {
         "jwt":req.cookies["_auth_t"]
     }
-    amqpClient.sendMessage(JSON.stringify(req.body),"RequestComment",token).then(msg => {
+    const payload = {
+        comment_id: req.params['cid'],
+    }
+    amqpClient.sendMessage(JSON.stringify(payload),"RequestComment",token).then(msg => {
         if(msg.properties.headers.http_response === 200){
             const result = msg.content.toString();
             console.log("Received " + msg.content.toString());
@@ -28,7 +31,15 @@ router.get('/:cid', function (req, res) {
 // Update a specific comment
 router.put('/:cid', validJWT, function (req, res) {
     //UpdateComment
-    amqpClient.sendMessage(JSON.stringify(req.body), "UpdateComment", {jwt:req.jwt}).then(msg => {
+    var token = {
+        "jwt":req.cookies["_auth_t"]
+    }
+    const payload = {
+        comment_id: req.params['cid'],
+        user_id: req.claims.sub,
+        content: req.body.content
+    }
+    amqpClient.sendMessage(JSON.stringify(req.body), "UpdateComment", token).then(msg => {
         if(msg.properties.headers.http_response === 200){
             console.log("Received " + msg.content.toString());
             const result = msg.content.toString(); // Maybe not parse
@@ -44,7 +55,14 @@ router.put('/:cid', validJWT, function (req, res) {
 // Delete a specific comment
 router.delete('/:cid', validJWT, function (req, res) {
     //DeleteComment
-    amqpClient.sendMessage(JSON.stringify(req.body), "DeleteComment", {jwt:req.jwt}).then(msg => {
+    var token = {
+        "jwt":req.cookies["_auth_t"]
+    }
+    const payload = {
+        comment_id: req.params['cid'],
+        user_id: req.claims.sub
+    }
+    amqpClient.sendMessage(JSON.stringify(req.body), "DeleteComment", token).then(msg => {
         if(msg.properties.headers.http_response === 200){
             const result = msg.content.toString(); // Maybe not parse
             console.log("Received " + msg.content.toString());
