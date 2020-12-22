87dc7a6403e1e4f01b1b3d21fbd1d5da87f9ad7f
Jakob Schøler <35096865+deadshotcalamity@users.noreply.github.com>
2020-12-22 17:16:54 +0100

    nicer log file

--- client/src/pages/automations/Scripts.js
+++ client/src/pages/automations/Scripts.js
@@ -70,7 +70,7 @@ class ScriptListingEntry extends Component {
         const handleDownload = (script_id) => {
             axios.post("/api/scripts/FindUserScript", {"user_script":script_id}).then(
                 (response) => {
-                    fileDownload(JSON.stringify(logs.map(log => `${log.created_at}: ${log.log}\n`)), "logs.txt")
+                    fileDownload(JSON.stringify(response.data.user_scripts.logs.map(log => `${log.created_at}: ${log.log}\n`)), "logs.txt")
                 }
             )
         };
@@ -88,6 +88,7 @@ class ScriptListingEntry extends Component {
         return <Row>
             <Col xs={1}>{this.props.main_file}</Col>
             <Col>{this.props.language}</Col>
+
             <Col>
                 <Button variant="outline-primary" onClick={() => {
                     handleDownload(this.props._id)
