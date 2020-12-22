b8de6c2bd4a764085e51e7b342cc15df02a8c356
Christian <christian@skjerning.eu>
2020-12-22 06:14:57 +0100

    added static serving

--- api/index.js
+++ api/index.js
@@ -50,12 +50,12 @@ app.get('/ping', function(req, res) {
 });
 
 // Serve the static files from the React app
-//app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 // Handles any requests that don't match the ones above
-//app.get('*', (req, res) => {
-//    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-//});
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port).setTimeout(10 * 1000);
